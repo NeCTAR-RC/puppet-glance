dee845c12e3b53b5f86b05bbf3987ec94468377f
Sam Morrison <sorrison@gmail.com>
2013-02-20 16:41:26 +1100

    Convert variables to  use hiera
    Change-Id: Ia3035ed8c881b452313c14a3ca13d8d66b4149d8

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,11 @@
 class glance {
 
+  $keystone_host = hiera('keystone::host')
+  $keystone_protocol = hiera('keystone::protocol')
+  $keystone_service_tenant = hiera('keystone::service_tenant')
+  $keystone_user = hiera('glance::keystone_user')
+  $keystone_password = hiera('glance::keystone_password')
+
   user { 'glance':
     groups => $ssl_group,
   }
