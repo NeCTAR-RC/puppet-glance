4ace7591f390eda45315abc8c474f82995ef93df
Sam Morrison <sorrison@gmail.com>
2013-03-28 15:45:05 +1100

    Push out nagios check
    Change-Id: Ib59af58b988073e3bfcc9b0cde6e23b794fa042c

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,7 +16,15 @@ class glance::nagios-checks {
   # These are checks that can be run by the nagios server.
   nagios::command {
     'check_glance':
-      check_command => '/usr/lib/nagios/plugins/check_glance.py --auth_url \'$ARG1$\' --username \'$ARG2$\' --password \'$ARG3$\' --tenant \'$ARG4$\' --req_count \'$ARG5$\' --req_images \'$ARG6$\''
+      check_command => '/usr/local/lib/nagios/plugins/check_glance.py --auth_url \'$ARG1$\' --username \'$ARG2$\' --password \'$ARG3$\' --tenant \'$ARG4$\' --req_count \'$ARG5$\' --req_images \'$ARG6$\''
+  }
+
+  file {'/usr/local/lib/nagios/plugins/check_glance.py':
+    ensure => present,
+    owner  => root,
+    group  => root,
+    mode   => '0775',
+    source => 'puppet:///modules/glance/check_glance.py',
   }
 }
 
