c29c224828155979300295e009edf068f98b35dc
Sam Morrison <sorrison@gmail.com>
2015-07-21 09:31:55 +1000

    Added develop class
    Change-Id: Id0d51bed9ccaf03e94905b28b9c17f897a8723d1

---
+++ manifests/develop.pp
@@ -0,0 +1,53 @@
+class glance::develop($branch='stable/juno') inherits glance {
+
+  include memcached
+
+  file {['/etc/glance', '/var/log/glance', '/var/lib/glance']:
+    ensure => directory,
+    owner  => glance,
+    group  => glance,
+  }
+
+  User['glance'] {
+    home => '/var/lib/glance',
+  }
+
+  git::clone { 'glance':
+    git_repo        => 'https://github.com/openstack/glance.git',
+    projectroot     => '/opt/glance',
+    branch          => $branch,
+  }
+
+  include glance::develop::api
+  include glance::develop::registry
+
+}
+
+class glance::develop::api inherits glance::api {
+  Package['glance-api'] {
+    ensure => absent,
+  }
+  Service['glance-api'] {
+    provider => upstart,
+  }
+
+  file {'/etc/init/glance-api.conf':
+    source => 'puppet:///modules/glance/api-init.conf',
+  }
+
+}
+
+class glance::develop::registry inherits glance::registry {
+  Package['glance-registry'] {
+    ensure => absent,
+  }
+
+  Service['glance-registry'] {
+    provider => upstart,
+  }
+
+  file {'/etc/init/glance-registry.conf':
+    source => 'puppet:///modules/glance/registry-init.conf',
+  }
+
+}
